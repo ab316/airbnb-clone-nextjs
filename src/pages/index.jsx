3912c70441f1e086df640081bc4d8c12d550c04b
Abdullah Baig <abdullahbaig456@gmail.com>
2022-03-11 21:14:53 +0100

    Add header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,18 @@
 import Head from 'next/head';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Head>
         <title>Airbnb</title>
         <meta name="description" content="Airbnb Clone" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
+      <Banner />
     </div>
   );
 }
